62dbe9d8d5f0b02ea121fdfc56e137ed423ba815
Shailesh <68338746+shailesh-ss-19-11@users.noreply.github.com>
2023-09-05 13:36:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+
+// data types in js 
+// number 
+// string 
+// Boolean 
+// undefined
+// null
+
+// types of variables in js 
+
+// const 
+// let 
+// var
+
+// const num1 = 10;
+// num1 = 20
+// console.log(num1,"this is constant varibale ")
+
+// let num1  = 10;
+// num1=20
+
+// // const num1 = 10;
+// console.log(num1,"this is local variable")
+
+// var num1 =10;
+
+// var num1=10;
+// console.log(num1,"this is GLOBALE variable")
+
+// const name = "shailesh";
+// const age = 24;
+// const address = "nagpur";
+// const isage24 = true;
+// console.log("my name is"+" " +name +"and age is"+" "+age+" I live in "+address );
+// console.log(`my name is ${name} and age is ${age} I live in ${address}`);
+
+// const myinfo = document.querySelector("#myinfo");
+// myinfo.innerText = `my name is ${name} and age is ${age} I live in ${address}`
+// myinfo.style.color = "red"
+
+// operators in js
+// arithmatic operator(+,-,*,/,%) 
+// const num1 = 10;
+// const num2 = 20;
+
+// const num3 = num1 + num2;
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+
+// logical operator (&&, || , !);--------> true false
+// && --> when both conditions are true ----> true 
+// || --> when both conditions are false ----> false 
+// ! --> true -> false , false ->true
+
+// const name1 = "shailesh"
+// const name2 = "vaibhav"
+
+// const dummy = "xyz";  //----false
+// const value = false;
+
+// console.log(name1=="vaibhav" && name2=="shailesh"); -->fasle
+
+// console.log(name1=="vaibhav" || name2=="shailesh");
+                // false               false---------------false 
+// console.log(name1=="shailesh" || name2=="rani");
+                // true                false   ---------------true 
+// console.log(!dummy);
+
+// ternary operator 
+//  condition ? statement    :  statement 
+
+// const name1 = "shailesh"
+// const name2 = "vaibhav"
+// name1=="vaibhav" ? console.log("ha sahi hai") :console.log("galat hai");
+// true              ?  executable                : not executable
+
+
+// conditional operator --------> true false
+// const num1 = 10;
+// const num2 = 20;
+
+// const num3 = "10"
+
+// const value1 = true
+// const value2 = "true"
+
+// console.log(num1>num2)  //false
+// console.log(num1<num2)  //true
+// console.log(num1<=num2)  //true    (num1<num2 || num1==num2)
+// console.log(num1>=num2)  //false    (num1>num2 || num1==num2)
+// console.log(num1==num2)  //false    
+// console.log(num1!=num2)  //false    
+// console.log(num1===num2)  //false ---> it checks value and data types 
+// console.log(num1==num3)  //true ---> it checks value only 
+// console.log(num1===num3)  //false ---> it checks value and data types
+
+
+// console.log(value1==value2)  //true ---> it checks value only 
+// console.log(value1===value2)  //false ---> it checks value and data types 
+
+
+
+// consditional statements
+// if-else
+// for loop
+// foreach
+// switch
+// while loop
+// do-while-loop
